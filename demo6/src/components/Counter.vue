2f82f7fa87b7a744b35e7ea0213c68048b34c43a
zhengwei <zhengwei1949@qq.com>
2018-01-01 20:41:50 +0800

    a

---
+++ demo6/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div><button @click="inc">+</button></div>
+    <div><button @click="dec">-</button></div>
+    <h3>{{num}}</h3>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return {
+          msg:'hello vue'
+      }},
+      methods:{
+          inc(){
+              this.$emit('inc');
+          },
+          dec(){
+              this.$emit('dec');
+          }
+      },
+      props:["num"]
+  }
+
+</script>
+
+<style>
+
+</style>
+
---
+++ demo6/src/views/GoodsList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      这是goodslist
+      <span>{{$route.params}}</span>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
+
+
